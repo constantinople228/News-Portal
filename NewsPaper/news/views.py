376cb25b8fcfa9527ca750c29b1b7a87b04d6d6d
constantinople228 <slobodskoi2007@gmail.com>
2024-09-20 19:45:01 +0300

    Add files via upload

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,103 @@
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from .filters import PostFilter
+from django.shortcuts import redirect
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+# Create your views here.
+class PostsList(ListView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+    ordering = '-time_create'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostCreate(CreateView, PermissionRequiredMixin):
+    permission_required = 'news.add_post'
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if self.request.path == '/articles/create/':
+            post.post_type = 'AR'
+        post.save()
+        return super().form_valid(form)
+
+
+class PostUpdate(UpdateView, PermissionRequiredMixin):
+    permission_required = 'news.change_post'
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView, PermissionRequiredMixin):
+    permission_required = 'news.delete_post'
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('search')
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "user_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    premium_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        premium_group.user_set.add(user)
+    return redirect('/')
